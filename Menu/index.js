ec956d60221908191a927f3545753cb7bb786b03
alex haszard <aggis.haszard@gmail.com>
2020-05-28 18:46:41 +1200

    added menu for morning tea

--- Menu/index.js
+++ Menu/index.js
@@ -1,12 +1,30 @@
 let morningTeaItems = Object.values(menu.content.morningTea);
+let morningTeaCards = [];
 let morningTeaNames = [];
+let morningTeaPrices = [];
+let morningTeaButtons = [];
 let menuDiv = document.getElementById("menuItems");
 
 for (let i = 0; i < morningTeaItems.length; i++) {
+  morningTeaCards[i] = document.createElement("div");
+  morningTeaCards[i].classList = "card border-success mb-3 menuCards";
+  menuDiv.appendChild(morningTeaCards[i]);
+
   morningTeaNames[i] = document.createElement("p");
   morningTeaNames[i].innerHTML = morningTeaItems[i].name;
-  morningTeaNames[i].classList = "defaultText";
-  document.body.insertBefore(morningTeaNames[i], menuDiv);
+  morningTeaNames[i].classList = "itemName card-header";
+  morningTeaCards[i].appendChild(morningTeaNames[i]);
+
+  morningTeaPrices[i] = document.createElement("p");
+  morningTeaPrices[i].innerHTML = "Price: $" + morningTeaItems[i].price;
+  morningTeaPrices[i].classList = "priceText";
+  morningTeaCards[i].appendChild(morningTeaPrices[i]);
+
+  morningTeaButtons[i] = document.createElement("button");
+  morningTeaButtons[i].innerHTML = "Add to Cart";
+  morningTeaButtons[i].classList = "btn btn-success addToCart";
+  morningTeaCards[i].appendChild(morningTeaButtons[i]);
+
   console.log(morningTeaItems[i].name);
   console.log(morningTeaItems[i].price);
 }
--- Menu/menu.js
+++ Menu/menu.js
@@ -9,7 +9,32 @@ let menu = {
 
       savouryPinwheel: {
         name: "Savoury Pinwheel",
-        price: "3.5",
+        price: "3",
+      },
+
+      muffins: {
+        name: "Muffins - Savoury or Sweet",
+        price: "3",
+      },
+
+      cinnamonSwirl: {
+        name: "Cinnamon Swirl",
+        price: "3",
+      },
+
+      sliceBrownie: {
+        name: "Slice/Brownie",
+        price: "3",
+      },
+
+      cheesePulls: {
+        name: "Cheese Pulls",
+        price: "3",
+      },
+
+      sandwiches: {
+        name: "Sandwiches",
+        price: "4",
       },
     },
   },
